58ad660f89348c682f9b45482766751e923b147c
jsebas337 <141984387+jsebas337@users.noreply.github.com>
2023-08-29 22:19:38 -0500

    Delete EJERCICIOSIMPLE2.py

--- EJERCICIOSIMPLE2.py
+++
@@ -1,5 +0,0 @@
-# Solicitar al usuario que ingrese su nombre
-nombre = input("Ingrese su nombre: ")
-
-# Mostrar el saludo acompañado del nombre del usuario
-print("¡Hola,", nombre, "! Bienvenido/a.")
